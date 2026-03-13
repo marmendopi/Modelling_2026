a303a03aa400a7dc54edf939962901951d212744
marmendozapinero <marmendozapinero@gmail.com>
2026-03-13 13:09:49 +0100

    added more

--- point.py
+++ point.py
@@ -17,6 +17,8 @@ class Point:
         """
         return f"({self.x}, {self.y})"
     def __repr__(self):
+        return self.__str__()
+
 
     def distance_origin(self):
         """
@@ -37,21 +39,21 @@ class Point:
         :param other: the other point to comapre against
         :return: True or False
         """
-
-p1 = Point(1, 2)
-p2 = Point(3, 4)
-print(p1.x, p1.y)
-print(p2.x, p2.y)
-print(p1, p2) # <1, 2>
-print(f"{p2} distance to origin is {p2.distance_origin()}")
-p3 = Point(12, 5)
-print(f"{p3} distance to origin is {p3.distance_origin()}")
-p1 = Point(6, 10)
-p2 = Point(6, 15)
-print(f"distance between {p1} and {p2} is {p1.distance_to(p2)}")
-p4 = Point(1,1)
-points = [p1, p2, p3]
-print(points)
-points.sort()
-print(points)
+if __name__ == "__main__": #Protects the code from import runs!!
+    p1 = Point(1, 2)
+    p2 = Point(3, 4)
+    print(p1.x, p1.y)
+    print(p2.x, p2.y)
+    print(p1, p2) # <1, 2>
+    print(f"{p2} distance to origin is {p2.distance_origin()}")
+    p3 = Point(12, 5)
+    print(f"{p3} distance to origin is {p3.distance_origin()}")
+    p1 = Point(6, 10)
+    p2 = Point(6, 15)
+    print(f"distance between {p1} and {p2} is {p1.distance_to(p2)}")
+    p4 = Point(1,1)
+    points = [p1, p2, p3]
+    print(points)
+    points.sort()
+    print(points)
 
