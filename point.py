71fc3ab16f34ce72143a1d449d016ceb78f7507c
marmendozapinero <marmendozapinero@gmail.com>
2026-03-13 12:50:59 +0100

    added more

--- point.py
+++ point.py
@@ -16,12 +16,42 @@ class Point:
         :return: String representation of Point class
         """
         return f"({self.x}, {self.y})"
+    def __repr__(self):
+
+    def distance_origin(self):
+        """
+        calculates the distance between point and origin
+        :return: float, distance between point and origin.
+        """
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+    def distance_to(self, point):
+        """
+        calculates the distance between current point and another point
+        :param point: the other point to calculate distance to
+        :return: float, distance between current point and another point.
+        """
+        return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
+    def __lt__(self, other):
+        """
+        retuns TRue if self is larger than other point
+        :param other: the other point to comapre against
+        :return: True or False
+        """
 
 p1 = Point(1, 2)
 p2 = Point(3, 4)
-
-
 print(p1.x, p1.y)
 print(p2.x, p2.y)
 print(p1, p2) # <1, 2>
+print(f"{p2} distance to origin is {p2.distance_origin()}")
+p3 = Point(12, 5)
+print(f"{p3} distance to origin is {p3.distance_origin()}")
+p1 = Point(6, 10)
+p2 = Point(6, 15)
+print(f"distance between {p1} and {p2} is {p1.distance_to(p2)}")
+p4 = Point(1,1)
+points = [p1, p2, p3]
+print(points)
+points.sort()
+print(points)
 
